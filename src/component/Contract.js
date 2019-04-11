98caa410ff707a4d9b5c0435ff2fa7a463073845
Jeoffrey <jeoff.howe@gmail.com>
2019-04-11 17:35:38 -0600

    add email function

--- src/component/Contract.js
+++ src/component/Contract.js
@@ -41,19 +41,20 @@ class Contract extends Component {
         let displayArray = this.state.contractArray.map((elem, index) => {
             return <div className='Contact' key={index}>
                 <div className="info">
+                <i class="fas fa-users fa-10x"></i>
                     <h2>Name: {elem.name}</h2>
                     <h3>Company: {elem.company}</h3>
                     <h4>Experience: {elem.experience}</h4>
                     <h4>Special Skills: {elem.skills + ' , '}</h4>
-                    <h4>Rate:{elem.rate}</h4>
+                    <h4>Rate: {elem.rate}</h4>
                 </div>
                 <div className="navbutton">
-                    <button className='delete' onClick={() => this.deleteUser(elem.id)}>Delete</button>
-                    <Popup trigger={<button className='edits'>Edit</button>} position="right center">
+                    <span className='button' onClick={() => this.deleteUser(elem.id)}>Delete</span>
+                    <Popup trigger={<span className='button'>Edit</span>} position="right center">
                         <div><Update contact={elem} index={index} update={this.updateContact} /></div>
                     </Popup>
                     <Popup trigger={<button className='contactInfo'>Contact</button>} position="right center">
-                        <div><h3>Email:</h3>{elem.email}
+                        <div><h3>Email:</h3><a href={`mailto:${elem.email}?Subject=Video Production Job`} target="_top">{elem.email}</a>
                             <h3>Phone:</h3>{elem.phone}
                             <h3>City:</h3>{elem.city}
                         </div>
@@ -72,8 +73,10 @@ class Contract extends Component {
                 <div className="diplayContacts">
                 {displayArray}
                 </div>
-                <div className="right">Find a Pro</div>
-                <Popup trigger={<button className="newContact">New Contact</button>} position='right center'>
+                <div className="right">Find A Pro
+                
+                </div>
+                <Popup trigger={<button className="newContact">New Contact</button>} position='left center'>
                     <div className='addNew'>
                         <h3>New Contact</h3>
                         <Create addNew={this.addNew} />
